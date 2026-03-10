3dc17e91c58f510a476325b65ef233a6a6ea910a
Murat <muratsat@users.noreply.github.com>
2026-03-10 11:03:20 +0600

    new raycast script

---
+++ raycast/scripts/toggle-dark-theme.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Toggle Dark theme
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🌙
+
+# ctrl + cmd - d : /Users/murat/.config/tmux/scripts/toggle-dark-theme
+~/.config/tmux/scripts/toggle-dark-theme
+
